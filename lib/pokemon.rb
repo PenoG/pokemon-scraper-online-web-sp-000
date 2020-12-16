4f2f5025a12483a15973ec87457fedc435e484e8
Mason Phillips <mjphillips2000@gmail.com>
2020-12-16 15:29:31 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,22 @@
+
+
 class Pokemon
+  attr_accessor :id, :name, :type, :db, :hp
+  
+  def initialize(id:, 
+    name:, 
+    type:, 
+    db:,
+    hp: nil)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+    @hp = hp
+  end
+  
+  
+  def self.save(name, type, id)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
 end
